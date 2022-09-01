4c14e2c1ec7ef43adb5972dfeb3265f4a0ce6e9a
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-09-01 20:49:19 +0200

    project structure

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const SIGNIN_PATH = '/signin';
+export const SIGNUP_PATH = '/signup';
+export const PROFILE_SETTING_PATH = '/profile-setting';
+export const PASSWORD_SETTING_PATH = '/password-setting';
+export const MESSENGER_PATH = '/forum';
+export const LEADERBOARD_PATH = '/leaderboard';
+export const GAME_PATH = '/game';
+export const ERROR404_PATH = '/404';
+export const ERROR500_PATH = '/500';
+export const ROOT_DIV = '#root';
