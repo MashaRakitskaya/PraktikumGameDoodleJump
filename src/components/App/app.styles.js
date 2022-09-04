2fe948fc3cc9650d224929cfbc374ff618661bf1
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-09-04 21:32:22 +0200

    changes: add styled AppWrapper and AppWrapperContainer in App

--- src/components/App/app.styles.js
+++ src/components/App/app.styles.js
@@ -1,5 +1,14 @@
 import styled from '@emotion/styled';
 
 export const AppWrapper = styled.div`
-  background: ${(props) => props.color};
+  height: 100vh;
+  overflow: auto;
+  background-color: var(--main-bg-color);
+`;
+export const AppWrapperContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  min-width: 320px;
+  min-height: 100vh;
+  margin: 0 auto;
 `;
