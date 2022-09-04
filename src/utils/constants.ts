2186880eebd0d0a045232b54002295d4aa322217
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-09-04 21:28:41 +0200

    add: constants.ts PATHs and leaderboardList

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,138 @@ export const SIGNIN_PATH = '/signin';
 export const SIGNUP_PATH = '/signup';
 export const PROFILE_SETTING_PATH = '/profile-setting';
 export const PASSWORD_SETTING_PATH = '/password-setting';
-export const MESSENGER_PATH = '/forum';
+export const FORUM_PATH = '/forum';
 export const LEADERBOARD_PATH = '/leaderboard';
 export const GAME_PATH = '/game';
 export const ERROR404_PATH = '/404';
 export const ERROR500_PATH = '/500';
 export const ROOT_DIV = '#root';
+
+export const leaderboardList = [
+  {
+    id: '1',
+    title: 'Elena List',
+    points: 1246434,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '2',
+    title: 'Elena List',
+    points: 100460,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '11',
+    title: 'Elena List',
+    points: 123224,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '21',
+    title: 'Elena List',
+    points: 104533200,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '111',
+    title: 'Elena List',
+    points: 1454234,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '211',
+    title: 'Elena List',
+    points: 1053500,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '12',
+    title: 'Elena List',
+    points: 1235634,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '22',
+    title: 'Elena List',
+    points: 1007740,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '122',
+    title: 'Elena List',
+    points: 12324,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '2222',
+    title: 'Elena List',
+    points: 10034420,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '1244',
+    title: 'Elena List',
+    points: 167234,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '2555',
+    title: 'Elena List',
+    points: 100240,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '1676',
+    title: 'Elena List',
+    points: 124634,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '268',
+    title: 'Elena List',
+    points: 104200,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '17',
+    title: 'Elena List',
+    points: 134234,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '288',
+    title: 'Elena List',
+    points: 104600,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+  {
+    id: '1655',
+    title: 'Elena List',
+    points: 123456,
+    urlImg:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=417&q=80'
+  },
+  {
+    id: '2355667',
+    title: 'Elena List',
+    points: 10009,
+    urlImg:
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  }
+];
