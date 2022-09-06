9d0b6a817e6e279fefd71ac0cac80887c6134b4c
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-09-06 18:23:45 +0200

    changes: add App.tsx  <Route path={FORUM_PATH} element={<Forum />} />           <Route path={FORUM_CHAT_ID_PATH} element={<ForumChat />} />

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import Leaderboard from '../../pages/Leaderboard/Leaderboard';
 import Sidebar from '../Sidebar/Sidebar';
 import { AppWrapper, AppWrapperContainer } from './app.styles';
-import { LEADERBOARD_PATH } from '../../utils/constants';
+import {
+  LEADERBOARD_PATH,
+  FORUM_PATH,
+  FORUM_CHAT_ID_PATH
+} from '../../utils/constants';
+import Forum from '../../pages/Forum/Forum';
+import ForumChat from '../ForumChat/ForumChat';
 
 function App() {
   return (
@@ -13,6 +19,8 @@ function App() {
         <Sidebar />
         <Routes>
           <Route path={LEADERBOARD_PATH} element={<Leaderboard />} />
+          <Route path={FORUM_PATH} element={<Forum />} />
+          <Route path={FORUM_CHAT_ID_PATH} element={<ForumChat />} />
         </Routes>
       </AppWrapperContainer>
     </AppWrapper>
