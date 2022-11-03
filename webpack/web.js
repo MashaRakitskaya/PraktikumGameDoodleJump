1b0ada823860aaefe22c529a6e3263a32f55842c
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-11-03 10:36:42 +0100

    changes: add comment in web.js webpack  output.library: 'ClientWebpack'

--- webpack/web.js
+++ webpack/web.js
@@ -1,36 +1,38 @@
-const path = require("path");
-const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+const path = require('path');
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  name: "web",
-  mode: "development",
-  target: "web",
-  entry: "./src/index.tsx",
+  name: 'web',
+  mode: 'development',
+  target: 'web',
+  entry: './src/index.tsx',
+  //library: "ClientWebpack"  означает что мы сможем обратится к собранному файлу
+  //"./src/index.tsx" --> "browserClient.js" как объекту и вызвать его функции
   output: {
-    filename: "browserClient.js",
-    path: path.join(__dirname, "../build/public"),
-    libraryTarget: "var",
-    library: "ClientWebpack",
+    filename: 'browserClient.js',
+    path: path.join(__dirname, '../build/public'),
+    libraryTarget: 'var',
+    library: 'ClientWebpack'
   },
   //  resolve: { modules: ["src", "node_modules"] }, искать в папках этих
   resolve: {
-    modules: ["src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".json", ".ts", ".tsx", ".css"],
-    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.json', '.ts', '.tsx', '.css'],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: { loader: "babel-loader" },
+        use: { loader: 'babel-loader' }
       },
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack", "file-loader"],
-      },
-    ],
-  },
+        use: ['@svgr/webpack', 'file-loader']
+      }
+    ]
+  }
 };
