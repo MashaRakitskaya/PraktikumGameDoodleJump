b6388205b7145a9011f260d9974781a8bfab71c3
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-11-23 16:11:02 +0100

    changes: add https:// and delete :3000

--- webpack/web.js
+++ webpack/web.js
@@ -3,7 +3,7 @@ const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
 const webpack = require('webpack');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-const BASE_URL = 'doodlers-doodlejump-17.ya-praktikum.tech:3000';
+const BASE_URL = 'https://doodlers-doodlejump-17.ya-praktikum.tech';
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
